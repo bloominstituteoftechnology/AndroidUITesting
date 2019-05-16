d89e3b5a43961e81ce02b8b5c01acf0dc07d88c8
Shoonsk8 <shoon@att.net>
2019-05-16 15:14:59 -0400

    initial

---
+++ app/src/main/java/com/shoon/androiduitesting/Calculator.java
@@ -0,0 +1,382 @@
+package com.shoon.androiduitesting;
+
+import java.util.ArrayList;
+
+public class Calculator {
+    public Calculator(float fX, float fY, float fZ) {
+        this.fX = fX;
+        this.fY = fY;
+        this.fZ = fZ;
+    }
+
+    private float fX,fY,fZ;
+    private String strLog,strNumber,strResult;
+    ArrayList<String> alstrCalc;
+    ArrayList<String> alstrOld;
+    ArrayList<String> alstrBracket;
+
+    public Calculator(){
+        strLog="";
+        strNumber="";
+        this.alstrCalc=new ArrayList<String>( 1 );
+    }
+
+    private boolean isAfterEaual=false;
+
+
+    private void setOperator(char cOperator){
+        if(isAfterEaual){
+            alstrCalc.set(alstrCalc.size()-1,  strNumber );
+        }else{
+            alstrCalc.add( strNumber );
+        }
+        strNumber="";
+        alstrCalc.add(Character.toString(   cOperator) );
+        strLog+=cOperator;
+        isAfterEaual=false;
+    }
+
+    public void addSymbol(char chrX){
+
+        switch (chrX){
+            case '/':
+                setOperator( chrX );
+                break;
+            case '*':
+                setOperator( chrX );
+                break;
+
+            case '-':
+                setOperator( chrX );
+                break;
+            case '+':
+                setOperator( chrX );
+                break;
+            case '%':
+                setOperator( chrX );
+                break;
+            case 'U': //undo
+                int iSize=alstrCalc.size();
+                if(iSize==0){
+                    alstrCalc= (ArrayList<String>) alstrOld.clone();
+
+                }else{
+                    alstrCalc.remove( alstrCalc.size()-1);
+                }
+                strLog+=chrX;
+                break;
+            case '(':
+                setOperator( chrX );
+                break;
+            case ')':
+                setOperator( chrX );
+                break;
+            case 'J':
+                setOperator( chrX );
+                break;
+            case 'R':
+                setOperator( chrX );
+                break;
+            case 'P':
+                alstrCalc.add( "PIE" );
+                strLog+=chrX;
+                break;
+            case 'r':
+                alstrCalc.add( "RAD" );
+                strLog+=chrX;
+                break;
+            case 'k':
+                setOperator( chrX );
+//                alstrCalc.add( "Cos(" );
+                break;
+            case 's':
+                setOperator( chrX );
+//                alstrCalc.add( "Sin(" );
+                break;
+            case 't':
+                setOperator( chrX );
+//                alstrCalc.add( "Tan(" );
+                break;
+            case 'h':
+                setOperator( chrX );
+                break;
+            case 'l':
+                setOperator( chrX );
+//                alstrCalc.add( "ArcCos(" );
+                break;
+            case 'w':
+//                alstrCalc.add( "ArcSin(" );
+                setOperator( chrX );
+                break;
+            case 'g':
+                setOperator( chrX );
+//                alstrCalc.add( "ArcTan(" );
+                break;
+            case 'M':
+                alstrCalc.add( "Matrix" );
+                strLog+=chrX;
+                break;
+
+            case 'Z':
+                alstrOld= (ArrayList<String>) alstrCalc.clone();
+                alstrCalc.clear();
+                strLog="";
+                strNumber="";
+                isAfterEaual=false;
+
+                break;
+            case '=':
+                if(strNumber==""){
+                    strResult="cannot end with operator";
+                    break;
+                }
+                if(isAfterEaual){
+                    alstrCalc.set(alstrCalc.size()-1, strNumber );                }else{
+                    alstrCalc.add( strNumber );
+                }
+
+
+                if(alstrCalc.get( alstrCalc.size()-1 )=="+"){
+                    strResult="cannot end with operator";
+
+                    break;
+                }
+                calculate( );
+                isAfterEaual=true;
+                break;
+
+            case 'A':
+            case 'a':
+                alstrCalc.add( "0xa" );
+                strLog+=chrX;
+                break;
+            case 'B':
+            case 'b':
+                alstrCalc.add( "0xb" );
+                strLog+=chrX;
+                break;
+            case 'C':
+            case 'c':
+                alstrCalc.add( "0xc" );
+                strLog+=chrX;
+                break;
+            case 'D':
+            case 'd':
+                alstrCalc.add( "0xd" );
+                strLog+=chrX;
+                break;
+            case 'E':
+            case 'e':
+                alstrCalc.add( "0xe" );
+                strLog+=chrX;
+                break;
+            case 'F':
+            case 'f':
+                alstrCalc.add( "0xf" );
+                strLog+=chrX;
+                break;
+
+            case '.':
+                if(strNumber.contains( "." ))break;
+            case '0':
+            case '1':
+            case '2':
+            case '3':
+            case '4':
+            case '5':
+            case '6':
+            case '7':
+            case '8':
+            case '9':
+
+                strNumber+=chrX;
+                strLog+=chrX;
+                break;
+            default:
+
+        }
+
+
+
+    }
+
+    public void addSymbol(String strX){
+        switch (strX){
+            case "C":
+                strLog="";
+                break;
+            case "=":
+                calculate( );
+                break;
+
+
+            default:
+                strLog+=strX;
+        }
+
+
+    }
+
+    private void calculate(){
+
+
+        if(alstrCalc.contains( "(" )){
+
+        }
+
+        alstrBracket= (ArrayList<String>) alstrCalc.clone();
+        strResult=calcInBracket( alstrBracket );
+
+
+
+    }
+
+
+    private String calcInBracket(ArrayList<String> x){
+        if(x.contains( "/" ))x=eliminateDivision( x );
+        if(x.contains( "*" ))x=eliminateMultiplication(x);
+        if(x.contains( "-" ))x=eliminateSubtraction( x );
+        if(x.contains( "+" ))x=eliminateAddition( x );
+
+
+        return x.get( 0);
+    }
+
+    private ArrayList<String> eliminateAddition(ArrayList<String> x){
+        for(int i=0;i<x.size();i++){
+            if(x.get(i).contains( "+")){
+                x.set( i-1,basicCalcString( x.get( i-1 ),"+",x.get( i+1 ) ));
+
+                try {
+                    x.remove( i );
+                } catch (Exception e) {
+                    e.getMessage();
+                }
+                try {
+                    x.remove( i );
+                } catch (Exception e){
+                    e.getMessage();
+                }
+                i=0;
+            }
+        }
+        return x;
+    }
+
+    private ArrayList<String> eliminateSubtraction(ArrayList<String> x){
+        for(int i=0;i<x.size();i++){
+            if(x.get(i)=="-"){
+
+                if(i==0){
+                    x.set( i,Float.toString( -Float.parseFloat( x.get( i+1 ))));
+                    for(i=i;i<x.size();i++) {
+                        x.set( i, x.get( i + 1 ) );
+                        if (i == x.size() - 2) i = 0;
+                    }
+
+                }else{
+                    x.set( i-1,basicCalcString( x.get( i-1 ),"-",x.get( i+1 ) ));
+                    for(i=i;i<x.size()-1;i++) {
+                        x.set( i, x.get( i + 2 ) );
+                        try {
+                            x.remove( x.size() - 1 );
+                        } catch (Exception e) {
+                            e.getMessage();
+                        }
+                        try {
+                            x.remove( x.size()-2 );
+                        } catch (Exception e){
+                            e.getMessage();
+                        }
+
+                        if (i == x.size() - 2) i = 0;
+                    }
+                }
+
+
+            }
+
+        }
+        return x;
+    }
+
+    private ArrayList<String> eliminateMultiplication(ArrayList<String> x){
+        for(int i=0;i<x.size();i++){
+            if(x.get(i)=="*"){
+                x.set( i,"*" );
+                x.set( i-1,basicCalcString( x.get( i-1 ),"*",x.get( i+1 ) ));
+                for(i=i;i<x.size()-1;i++){
+                    x.set( i,x.get( i+1 ));
+                    x.remove( x.size()-1 );
+                    x.remove( x.size()-2 );
+                    if(i==x.size()-2)i=0;
+                }
+            }
+
+        }
+        return x;
+    }
+
+    private ArrayList<String> eliminateDivision(ArrayList<String> x){
+        for(int i=0;i<x.size();i++){
+            if(x.get(i)=="/"){
+                x.set( i,"*" );
+                x.set( i+1,Float.toString( 1/Float.parseFloat(x.get( i+1 ) )) );
+            }
+
+        }
+        return x;
+    }
+    private String basicCalcString(String strA,String strOperation,String strB){
+        float a=Float.parseFloat( strA ),
+                b=Float.parseFloat( strB ),
+        fResult = 0;
+        switch (strOperation){
+            case "+":
+               fResult=addition( a,b );
+               break;
+            case "-":
+                fResult=subtraction( a,b );
+                break;
+            case "*":
+                fResult=multiplication( a,b );
+                break;
+            case "/":
+                fResult=division( a,b );
+                break;
+        }
+
+
+        return Float.toString(fResult);
+    }
+
+    private float addition(float a,float b){
+        return a+b;
+    }
+
+    private float subtraction(float a,float b){
+        return a-b;
+    }
+    private float multiplication(float a,float b){
+        return a*b;
+    }
+    private float division(float a,float b){
+        return a/b;
+    }
+
+    public String add(String strX,String strY){
+        fX=Float.parseFloat( strX);
+        fY=Float.parseFloat( strY);
+        fZ=fX+fY;
+        strResult=Float.toString( fZ );
+        return strResult;
+    }
+
+    public String showLog(){
+        return this.strLog;
+    }
+    public String showResult(){
+        return this.strResult;
+    }
+
+}
---
+++ app/src/main/java/com/shoon/androiduitesting/MainActivity.java
@@ -0,0 +1,268 @@
+package com.shoon.androiduitesting;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+
+
+public class MainActivity extends AppCompatActivity {
+    TextView inputText;
+    TextView textResult;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.activity_main );
+        final Calculator calculator=new Calculator(  );
+        inputText=findViewById( R.id.inputText );
+        textResult=findViewById( R.id.textResult );
+        findViewById( R.id.buttonEqual ).setOnClickListener(  new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                calculator.addSymbol( '=' );
+                textResult.setText( calculator.showResult() );
+
+            }
+        } );
+
+        findViewById( R.id.button0 ).setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                calculator.addSymbol( "0" );
+                inputText.setText( calculator.showLog() );
+
+            }
+        } );
+        findViewById( R.id.button1 ).setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                calculator.addSymbol( '1' );
+                inputText.setText( calculator.showLog() );
+
+            }
+        } );
+        findViewById( R.id.button2 ).setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                calculator.addSymbol( '2' );
+                inputText.setText( calculator.showLog() );
+
+            }
+        } );
+        findViewById( R.id.button3 ).setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                calculator.addSymbol( '3' );
+                inputText.setText( calculator.showLog() );
+
+            }
+        } );
+        findViewById( R.id.button4 ).setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                calculator.addSymbol( '4' );
+                inputText.setText( calculator.showLog() );
+            }
+        } );
+        findViewById( R.id.button5 ).setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                calculator.addSymbol( '5' );
+                inputText.setText( calculator.showLog() );
+            }
+        } );
+        findViewById( R.id.button6 ).setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                calculator.addSymbol( '6' );
+                inputText.setText( calculator.showLog() );
+            }
+        } );
+        findViewById( R.id.button7 ).setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                calculator.addSymbol( '7' );
+                inputText.setText( calculator.showLog() );
+            }
+        } );
+        findViewById( R.id.button8 ).setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                calculator.addSymbol( '8' );
+                inputText.setText( calculator.showLog() );
+            }
+        } );
+        findViewById( R.id.button9 ).setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                calculator.addSymbol( '9' );
+                inputText.setText( calculator.showLog() );
+            }
+        } );
+        findViewById( R.id.buttonDevide ).setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                calculator.addSymbol( '/' );
+                inputText.setText( calculator.showLog() );
+            }
+        } );
+        findViewById( R.id.buttonMultiply ).setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                calculator.addSymbol( '*' );
+                inputText.setText( calculator.showLog() );
+            }
+        } );
+        findViewById( R.id.buttonMinus ).setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                calculator.addSymbol( '-' );
+                inputText.setText( calculator.showLog() );
+            }
+        } );
+        findViewById( R.id.buttonPlus ).setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                calculator.addSymbol( '+' );
+                inputText.setText( calculator.showLog() );
+            }
+        } );
+        findViewById( R.id.buttonPeriod ).setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                calculator.addSymbol( '.' );
+                inputText.setText( calculator.showLog() );
+            }
+        } );
+        findViewById( R.id.buttonPercent ).setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                calculator.addSymbol( '%' );
+                inputText.setText( calculator.showLog() );
+            }
+        } );
+
+
+        findViewById( R.id.buttonClear ).setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                calculator.addSymbol( 'Z' );
+                inputText.setText("");
+
+            }
+        } );
+
+        findViewById( R.id.buttonUndo ).setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                calculator.addSymbol( 'U' );
+                inputText.setText( calculator.showLog() );
+            }
+        } );
+        findViewById( R.id.buttonLeftBracket ).setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                calculator.addSymbol( '(' );
+                inputText.setText( calculator.showLog() );
+            }
+        } );
+        findViewById( R.id.buttonRightBracket ).setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                calculator.addSymbol( ')' );
+                inputText.setText( calculator.showLog() );
+
+
+            }
+        } );
+        findViewById( R.id.buttonPoweredBy2 ).setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                calculator.addSymbol( 'J' );
+                inputText.setText( calculator.showLog() );
+
+            }
+        } );
+        findViewById( R.id.buttonSquareRoot ).setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                calculator.addSymbol( 'R' );
+                inputText.setText( calculator.showLog() );
+            }
+        } );
+        findViewById( R.id.buttonPie ).setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                calculator.addSymbol( 'P' );
+                inputText.setText( calculator.showLog() );
+            }
+        } );
+        findViewById( R.id.buttonRadian ).setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                calculator.addSymbol( 'r' );
+                inputText.setText( calculator.showLog() );
+            }
+        } );
+        findViewById( R.id.buttonCos ).setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                calculator.addSymbol( 'k' );
+                inputText.setText( calculator.showLog() );
+            }
+        } );
+        findViewById( R.id.buttonSin ).setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                calculator.addSymbol( 's' );
+                inputText.setText( calculator.showLog() );
+            }
+        } );
+        findViewById( R.id.buttonTan ).setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                calculator.addSymbol( 't' );
+                inputText.setText( calculator.showLog() );
+            }
+        } );
+        findViewById( R.id.buttonTheta ).setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                calculator.addSymbol( 'h' );
+                inputText.setText( calculator.showLog() );
+            }
+        } );
+        findViewById( R.id.buttonArcCos ).setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                calculator.addSymbol( 'l' );
+                inputText.setText( calculator.showLog() );
+            }
+        } );
+        findViewById( R.id.buttonArcSin ).setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                calculator.addSymbol( 'w' );
+                inputText.setText( calculator.showLog() );
+            }
+        } );
+        findViewById( R.id.buttonArcTan ).setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                calculator.addSymbol( 'g' );
+                inputText.setText( calculator.showLog() );
+            }
+        } );
+        findViewById( R.id.buttonMatrix ).setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                calculator.addSymbol( 'M' );
+                inputText.setText( calculator.showLog() );
+            }
+        } );
+
+
+        inputText.setText( calculator.showLog() );
+
+    }
+}
